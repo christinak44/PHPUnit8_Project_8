a8209c3f7bfb2aa09da498efd0b7d95a8e268542
Christina King <kingcm41@hotmail.com>
2020-02-23 20:43:18 -0500

    Login
    registration and login code complete

---
+++ inc/completeLogin.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__. '/bootstrap.php';
+
+$user = findUserByUsername(request()->get('username'));
+
+if (empty($user)) {
+  $session->getFlashBag()->add('error', 'Username was not found');
+  redirect('/login.php');
+}
+if (!password_verify(request()->get('password', $user['password']))) {
+  $session->getFlashBag()->add('error', 'Invalid Password');
+  redirect('/login.php');
+}
+
+$session->set('auth_logged_in', true);
+$session->set('auth_user_id', (int) $user['id']);
+
+$session->getFlashBag()->add('success', 'Successfully Logged In');
+redirect('/');
